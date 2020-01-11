fd468c7ef3e69ed2c624f10b0732c3e9e30ddf38
Beata Rudzka <beatarudzka89@gmail.com>
2020-01-11 20:24:32 +0100

    adding images and creating html

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -3,9 +3,14 @@ import React from 'react'
 
 function Skills() {
   return (
-    <div className="skills">
-      Skills
-    </div>
+    <section className="skills">
+      <div className="skill"></div>
+      <div className="skill">CSS</div>
+      <div className="skill">JavaScript</div>
+      <div className="skill">React</div>
+      <div className="skill">Gulp</div>
+      <div className="skill">Git</div>
+    </section>
   )
 }
 
