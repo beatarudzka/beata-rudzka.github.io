87922906a95649a1fad064e88fc2889a09633140
Beata Rudzka <beatarudzka89@gmail.com>
2020-01-12 13:22:54 +0100

    styling logos and adding texts

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,7 +4,12 @@ import React from 'react'
 function Projects() {
   return (
     <div className="projects">
-      Projects
+      <div className="project">Project__card 1</div>
+      <div className="project">Project__card 2</div>
+      <div className="project">Project__card 3</div>
+      <div className="project">Project__card 4</div>
+      <div className="project">Project__card 5</div>
+      <div className="project">Project__card 6</div>
     </div>
   )
 }
--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,65 +1,70 @@
 import React from 'react'
 
+import HTML from '../../img/html.png'
+import CSS from '../../img/css.svg'
+import JS from '../../img/js.svg'
+import REACT from '../../img/react.png'
+import GULP from '../../img/gulp.svg'
+import GIT from '../../img/git.png'
 
 function Skills() {
   return (
     <section className="skills">
       <div className="skill">
-        <img src="../../img/html.png" alt="html" className="skill_image" />
-        <h4 className="heading-4">
+        <img src={HTML} alt="html" className="skill__image" />
+        <h4 className="heading-4 heading-4--dark">
           HTML
         </h4>
-        <p className="skill_text">
+        <p className="skill__text">
           Staram się programować semantyczne strony, stosując obowiązujące standardy (HTML5) oraz metodologię BEM.
-          Ma to pozytywny wpływ na SEO (search engine optimimization), czyli na tak zwane pozycjonowanie naszej strony.
+          Ma to pozytywny wpływ na pozycjonowanie mojej strony.
         </p>
       </div>
       <div className="skill">
-        <img src="../../img/css.svg" alt="css" className="skill_image" />
-        <h4 className="heading-4">
+        <img src={CSS} alt="css" className="skill__image" />
+        <h4 className="heading-4 heading-4--dark">
           CSS
         </h4>
-        <p className="skill_text">
-          Moje projekty są responsywne a także stosuję nowoczesne modele projektowania flexbox czy grid.
+        <p className="skill__text">
+          Moje projekty są responsywne, a także stosuję nowoczesne modele projektowania flexbox czy grid.
           Do usprawnienia pracy ze stylowaniem używam preprocesora Sass.
         </p>
       </div>
       <div className="skill">
-        <img src="../../img/js.svg" alt="javascript" className="skill_image" />
-        <h4 className="heading-4">
+        <img src={JS} alt="javascript" className="skill__image" />
+        <h4 className="heading-4 heading-4--dark">
           JavaScript
         </h4>
-        <p className="skill_text">
+        <p className="skill__text">
           Dzięki znajomości Javascriptu jestem w stanie robić proste strony oraz aplikacje.
           W przyszłości, chcę się nauczyć Node.js do tworzenia backendu.
         </p>
       </div>
       <div className="skill">
-        <img src="../../img/react.png" alt="react" className="skill_image" />
-        <h4 className="heading-4">
+        <img src={REACT} alt="react" className="skill__image" />
+        <h4 className="heading-4 heading-4--dark">
           React.js
         </h4>
-        <p className="skill_text">
+        <p className="skill__text">
           Czuję się komfortowo tworząc projekty w tej bibliotece. Obecnie uczę się stosować Redux do aplikacji w React.js.
         </p>
       </div>
       <div className="skill">
-        <img src="gulp" alt="gulp" className="skill_image" />
-        <h4 className="heading-4">
+        <img src={GULP} alt="gulp" className="skill__image" />
+        <h4 className="heading-4 heading-4--dark">
           Gulp
         </h4>
-        <p className="skill_text">
+        <p className="skill__text">
           Do uruchomienia preprocesora CSS, jego minifikacji oraz automatycznego przeładowania strony używam Gulp4.
       </p>
       </div>
       <div className="skill">
-        <img src="../../img/git.png" alt="git" className="skill_image" />
-        <h4 className="heading-4">
+        <img src={GIT} alt="git" className="skill__image" />
+        <h4 className="heading-4 heading-4--dark">
           Git
         </h4>
-        <p className="skill_text">
-          Git jest moim niezbędnikiem w trakcie projektów grupowych.
-           Pojęcia takie jak merge, rebase czy checkout nie są mi obce.
+        <p className="skill__text">
+          Od samego początku nauki programowania służy mi do zapisywania postępu w kodzie oraz organizacji pracy zespołu nad jednym kodem.
         </p>
       </div>
     </section>
