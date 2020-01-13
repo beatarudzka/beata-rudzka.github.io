62185798710460360b10d31efd5c50e5de49b688
Beata Rudzka <beatarudzka89@gmail.com>
2020-01-13 13:36:58 +0100

    builing html and pre-styling ogf header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,18 @@
 import React from 'react'
 
-import LINKEDIN from '../../img/linkedin-logo.png'
+import LINKEDIN from '../../img/kindpng_4175039.png'
 import GITHUB from '../../img/GitHub_Logo_White.png'
 
 function Header() {
   return (
     <header className="header">
-      <h3 className="heading-3">
-        Junior Web Developer
-      </h3>
       <h1 className="heading-1">Beata Rudzka</h1>
-      <button className="btn">Więcej o mnie</button>
       <div className="header__link">Portale społecznościowe:</div>
       <div className="header__logo">
         <img src={GITHUB} alt="github logo" className="header__logo--github" />
         <img src={LINKEDIN} alt="linkedin logo" className="header__logo--linkedin" />
       </div>
+      <button className="btn header__btn">Więcej o mnie</button>
     </header>
   )
 }
