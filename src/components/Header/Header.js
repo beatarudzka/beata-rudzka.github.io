fa18ea486f29d9fe7362dfe6dceacf148d08a04c
Beata Rudzka <beatarudzka89@gmail.com>
2020-01-13 11:32:40 +0100

    adding typography and header structure

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,21 @@
 import React from 'react'
 
+import LINKEDIN from '../../img/linkedin-logo.png'
+import GITHUB from '../../img/GitHub_Logo_White.png'
 
 function Header() {
   return (
     <header className="header">
-      Header
+      <h3 className="heading-3">
+        Junior Web Developer
+      </h3>
+      <h1 className="heading-1">Beata Rudzka</h1>
+      <button className="btn">Więcej o mnie</button>
+      <div className="header__link">Portale społecznościowe:</div>
+      <div className="header__logo">
+        <img src={GITHUB} alt="github logo" className="header__logo--github" />
+        <img src={LINKEDIN} alt="linkedin logo" className="header__logo--linkedin" />
+      </div>
     </header>
   )
 }
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -4,7 +4,7 @@ import React from 'react'
 function Sidebar() {
   return (
     <div className="sidebar">
-      Sidebar
+
     </div>
   )
 }
